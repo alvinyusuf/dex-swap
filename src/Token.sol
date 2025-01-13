1bc12bdee16cabc3fefd2bba7c0245f066acfbb9
Alvin Yusuf <alvinyr32@gmail.com>
2025-01-13 10:53:39 +0700

    forge fmt

--- src/Token.sol
+++ src/Token.sol
@@ -7,12 +7,10 @@ import {Ownable} from "@openzeppelin-contracts-5.1.0/access/Ownable.sol";
 error Unauthorized();
 
 contract Token is ERC20, Ownable {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply,
-        address initialOwner
-    ) ERC20(name, symbol) Ownable(initialOwner) {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address initialOwner)
+        ERC20(name, symbol)
+        Ownable(initialOwner)
+    {
         _transferOwnership(initialOwner);
         _mint(initialOwner, initialSupply * (10 ** uint256(decimals())));
     }
